24236339d262eff41b3123e5929396805932f50f
p1ssguzzler <dake3626@gmail.com>
2024-10-31 22:29:49 -0500

    Add files via upload

---
+++ LAismylady.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+def open_rickroll():
+    # Rickroll link
+    rickroll_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    webbrowser.open(rickroll_url)
+
+if __name__ == "__main__":
+    open_rickroll()
